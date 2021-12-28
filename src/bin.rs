5701dde7ef6709b67fc73ea46a5d88ac46ea2eda
bsdinis <baltasar.dinis@tecnico.ulisboa.pt>
2021-12-28 20:54:52 +0000

    clap required arguments

--- src/bin.rs
+++ src/bin.rs
@@ -1,10 +1,18 @@
-use bencher::{BencherError, Config};
-use clap::{App, Arg, ArgMatches, SubCommand};
+use bencher::Config;
+use clap::{App, Arg, SubCommand};
 use cli_table::{format::Justify, Cell, Style, Table};
 use eyre::Result;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+
+    let config = Config::new()?;
+    let experiments = config.experiments();
+
+    let available_experiments = experiments
+        .iter()
+        .map(|e| e.exp_type.as_ref())
+        .collect::<Vec<&str>>();
 
     let app = App::new("bencher")
         .version("0.1")
@@ -13,121 +21,129 @@ fn main() -> Result<()> {
         .subcommand(
             SubCommand::with_name("list")
                 .about("list the available experiments")
-                .arg(Arg::with_name("experiment_type").help("the experiment to use")),
+                .arg(
+                    Arg::with_name("experiment_type")
+                        .help("the experiment to use")
+                        .required(true)
+                        .possible_values(&available_experiments),
+                ),
         )
         .subcommand(
             SubCommand::with_name("status")
                 .about("queries the database to see how many datapoints we have per test label")
-                .arg(Arg::with_name("experiment_type").help("the experiment to use")),
+                .arg(
+                    Arg::with_name("experiment_type")
+                        .help("the experiment to use")
+                        .required(true)
+                        .possible_values(&available_experiments),
+                ),
         )
         .subcommand(
             SubCommand::with_name("table")
                 .about("get the results for a specific experiment in tabular form")
-                .arg(Arg::with_name("experiment_type").help("the experiment to use")),
+                .arg(
+                    Arg::with_name("experiment_type")
+                        .help("the experiment to use")
+                        .required(true)
+                        .possible_values(&available_experiments),
+                ),
         )
         .subcommand(
             SubCommand::with_name("latex")
                 .about("get the results for a specific experiment in a latex table form")
-                .arg(Arg::with_name("experiment_type").help("the experiment to use")),
+                .arg(
+                    Arg::with_name("experiment_type")
+                        .help("the experiment to use")
+                        .required(true)
+                        .possible_values(&available_experiments),
+                ),
         )
         .subcommand(
             SubCommand::with_name("dat")
                 .about("get the results for a specific experiment in a .dat form")
-                .arg(Arg::with_name("experiment_type").help("the experiment to use"))
-                .arg(Arg::with_name("prefix").help("the prefix for the files")),
+                .arg(
+                    Arg::with_name("experiment_type")
+                        .help("the experiment to use")
+                        .required(true)
+                        .possible_values(&available_experiments),
+                )
+                .arg(
+                    Arg::with_name("prefix")
+                        .help("the prefix for the files")
+                        .required(true),
+                ),
         )
         .subcommand(
             SubCommand::with_name("gnuplot")
                 .about("get the gnuplot representation for an experiment")
-                .arg(Arg::with_name("experiment_type").help("the experiment to use"))
-                .arg(Arg::with_name("prefix").help("the prefix for the files")),
+                .arg(
+                    Arg::with_name("experiment_type")
+                        .help("the experiment to use")
+                        .required(true)
+                        .possible_values(&available_experiments),
+                )
+                .arg(
+                    Arg::with_name("prefix")
+                        .help("the prefix for the files")
+                        .required(true),
+                ),
         )
         .subcommand(
             SubCommand::with_name("add")
                 .about("add a new experiment")
-                .arg(Arg::with_name("experiment_type").help("the experiment type"))
-                .arg(Arg::with_name("experiment_label").help("the label for this experiment"))
-                .arg(Arg::with_name("experiment_code").help("the code for this experiment")),
+                .arg(
+                    Arg::with_name("experiment_type")
+                        .help("the experiment to use")
+                        .required(true)
+                        .possible_values(&available_experiments),
+                )
+                .arg(
+                    Arg::with_name("experiment_label")
+                        .help("the label for this experiment")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("experiment_code")
+                        .help("the code for this experiment")
+                        .required(true),
+                ),
         )
         .get_matches();
-
-    let config = Config::new()?;
 
     match app.subcommand() {
         ("list", Some(_)) => list(&config)?,
         ("status", Some(_)) => status(&config)?,
         ("table", Some(matches)) => {
             config
-                .get_experiment_handle(extract_experiment_type(&config, matches)?)
-                .unwrap()
+                .get_experiment_handle(matches.value_of("exp_type").unwrap())?
                 .dump_table()?;
         }
         ("latex", Some(matches)) => {
             config
-                .get_experiment_handle(extract_experiment_type(&config, matches)?)
-                .unwrap()
+                .get_experiment_handle(matches.value_of("exp_type").unwrap())?
                 .dump_latex_table()?;
         }
         ("dat", Some(matches)) => {
             config
-                .get_experiment_handle(extract_experiment_type(&config, matches)?)
-                .unwrap()
-                .dump_dat(matches.value_of("prefix").unwrap_or("xxx"))?;
+                .get_experiment_handle(matches.value_of("exp_type").unwrap())?
+                .dump_dat(matches.value_of("prefix").unwrap())?;
         }
         ("gnuplot", Some(matches)) => {
             config
-                .get_experiment_handle(extract_experiment_type(&config, matches)?)
-                .unwrap()
-                .dump_gnuplot(matches.value_of("prefix").unwrap_or("xxx"))?;
+                .get_experiment_handle(matches.value_of("exp_type").unwrap())?
+                .dump_gnuplot(matches.value_of("prefix").unwrap())?;
         }
         ("add", Some(matches)) => {
             config.add_experiment(
-                extract_experiment_type(&config, matches)?,
-                extract_experiment_label(matches)?,
-                extract_experiment_code(matches)?,
+                matches.value_of("experiment_type").unwrap(),
+                matches.value_of("experiment_label").unwrap(),
+                matches.value_of("experiment_code").unwrap(),
             )?;
         }
         _ => {}
     }
 
     Ok(())
-}
-
-fn extract_experiment_type<'a, 'b>(config: &'a Config, matches: &'b ArgMatches) -> Result<&'b str> {
-    let available_experiments = config
-        .experiments()
-        .into_iter()
-        .map(|e| e.exp_type)
-        .collect::<Vec<_>>();
-    Ok(match matches.value_of("experiment_type") {
-        None => Err(BencherError::MissingExperiment(
-            available_experiments.join(","),
-        ))?,
-        Some(label) => {
-            if config.has_experiment(label) {
-                label
-            } else {
-                Err(BencherError::ExperimentNotFound(
-                    label.to_string(),
-                    available_experiments.join(","),
-                ))?
-            }
-        }
-    })
-}
-
-fn extract_experiment_label<'a>(matches: &'a ArgMatches) -> Result<&'a str> {
-    Ok(match matches.value_of("experiment_label") {
-        None => Err(BencherError::MissingLabel)?,
-        Some(label) => label,
-    })
-}
-
-fn extract_experiment_code<'a>(matches: &'a ArgMatches) -> Result<&'a str> {
-    Ok(match matches.value_of("experiment_code") {
-        None => Err(BencherError::MissingCode)?,
-        Some(label) => label,
-    })
 }
 
 fn list(config: &Config) -> Result<()> {
